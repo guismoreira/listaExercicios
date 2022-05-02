2df471c0ce496a84202e55be479ac888cf2a286c
guismoreira <guilhermemoreira55@gmail.com>
2022-05-01 23:02:38 -0300

    ex 11

--- Ex11.java
+++ Ex11.java
@@ -1,9 +1,46 @@
 package listaExercicios;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+class BubbleSort {
+	
+	void bubbleSort(int bubbleArray[]) {
+		int num = bubbleArray.length;
+		for(int i=0; i<num-1; i++) {
+			for(int j=0; j<num-i-1; j++) {
+				if(bubbleArray[j] > bubbleArray[j+1]) {
+					int temp = bubbleArray[j];
+					bubbleArray[j] = bubbleArray[j + 1];
+					bubbleArray[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	void printArray(int vet[]) {
+		int n=vet.length;
+		for(int i=0; i<n; ++i){
+			System.out.println(vet[i]+" ");
+		System.out.println();
+		}
+	}
+}
+
 public class Ex11 {
 
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Random r = new Random();
+		BubbleSort b = new BubbleSort();
+		int vet[] = new int[100];
+		for(int i=0; i < vet.length; i++) {
+			vet[i] = r.nextInt();
+		}
+		b.bubbleSort(vet);
+		b.printArray(vet);
 
 	}
 
